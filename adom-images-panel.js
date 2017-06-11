4d7caec298e23050caaa9fd05df32a9c3e53893c
Adominguezes <alberto@adominguez.es>
2017-06-11 16:31:00 +0200

    feat(*): add carrousel type

--- adom-images-panel.js
+++ adom-images-panel.js
@@ -5,6 +5,9 @@
     is: 'adom-images-panel',
 
     properties: {
+      /**
+       * Array with items
+       */
       items: {
         type: Array,
         value: function () {
@@ -12,18 +15,42 @@
         },
         observer: '_computeItems'
       },
+      /**
+       * Number of columns in accordion
+       */
       columns: {
         type: Number,
         reflectToAttribute: true
       },
+      /**
+       * Type of panel to show: accordion | carrousel
+       */
       type: {
         type: String,
         value: 'accordion',
         reflectToAttribute: true
       },
+      /**
+       * Total heigth of panel
+       */
       height: {
         type: Number,
         value: 400
+      },
+      /**
+       * show an icon for left and right navigation
+       */
+      iconNavigation: {
+        type: String
+      },
+      /**
+       * set the selected image for carrousel type
+       */
+      selected: {
+        type: Number,
+        value: 0,
+        reflectToAttribute: true,
+        observer: '_computeSelected'
       }
     },
     _computeItems: function () {
@@ -31,6 +58,37 @@
         this.items[i].id = i
       }
       this.columns = this.items.length;
+      if ((this.type) && (this.type === 'carrousel')) {
+        this._computeSelected();
+      }
+    },
+    _computeSelected: function () {
+      if ((this.selected === (this.items.length - 1)) || ((this.type === 'accordion'))) {
+        this.$.next.hidden = true;
+      } else {
+        this.$.next.hidden = false;
+      }
+      if ((this.selected === 0) || (this.type === 'accordion')) {
+        this.$.before.hidden = true;
+      } else {
+        this.$.before.hidden = false;
+      }
+    },
+    _beforeButton: function () {
+      if (this.selected !== 0) {
+        this.selected = this.selected - 1;
+        this._translateCarrousel();
+      }
+    },
+    _nextButton: function () {
+      if (this.selected !== this.items.length) {
+        this.selected = this.selected + 1;
+        this._translateCarrousel();
+      }
+    },
+    _translateCarrousel: function () {
+      var translate = (100 / this.columns) * this.selected;
+      this.$.organizer.style = "transform: translateX(-" + translate + "%)"
     }
 
   });
